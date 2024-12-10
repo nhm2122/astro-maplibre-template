dc97793dc5ad1faf8c89e0dea639bce37fb38dc5
nhm2122 <nhm2122@columbia.edu>
2024-12-10 12:51:23 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://design-practices.github.io",
+  site: "https://nhm2122.github.io",
   base: "astro-maplibre-template",
   integrations: [mdx(), sitemap(), tailwind()],
 });
